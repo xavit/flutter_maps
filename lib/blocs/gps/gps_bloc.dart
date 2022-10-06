092ded8fc09d94ce9b128e227bf891381ea871bb
Javier Claros <javiclaros@gmail.com>
2022-10-06 14:32:30 -0400

    Setup iOS Permisos

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -20,5 +21,30 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
         ),
       );
     });
+
+    _init();
+  }
+
+  Future<void> _init() async {
+    final isGpsEnabled = await _checkGpsStatus();
+    print('isGpsEnabled: $isGpsEnabled');
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+
+    Geolocator.getServiceStatusStream().listen((status) {
+      final isGpsEnabled = (status.index == 1) ? true : false;
+      print("service status: $isGpsEnabled");
+      //TODO: Disparar eventos...
+    });
+
+    return isEnable;
+  }
+
+  @override
+  Future<void> close() {
+    // TODO: Limpiar el ServiceStatusStream
+    return super.close();
   }
 }
